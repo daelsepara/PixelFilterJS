309ba150bb4e685bcc055dfb0a736a3255bee895
Dael Separa <dael.separa@gmail.com>
2019-06-22 07:32:11 +0800

    formatting improvements

--- js/filters/nearest.js
+++ js/filters/nearest.js
@@ -1,6 +1,6 @@
 var Filter = class {
 
-    Apply(Input, srcx, srcy, scale, threshold) {
+	Apply(Input, srcx, srcy, scale, threshold) {
 
 		scale = Math.max(1, scale);
 
@@ -9,11 +9,13 @@ var Filter = class {
 		var src = this.ToArray(Input, srcx, srcy);
 		var dst = new Uint32Array(Common.SizeX * Common.SizeY);
 
-        this.nearestNeighborScale(src, srcx, srcy, srcx, dst, Common.SizeX, Common.SizeY, Common.SizeX, 0, srcy);
-        this.ToImage(Common.ScaledImage, dst, Common.SizeX, Common.SizeY);
-    }
-    
-    // helper functions added to work with PixelFilter
+		this.nearestNeighborScale(src, srcx, srcy, srcx, dst, Common.SizeX, Common.SizeY, Common.SizeX, 0, srcy);
+		this.ToImage(Common.ScaledImage, dst, Common.SizeX, Common.SizeY);
+
+		notify({ ScalingProgress: 1.0 });
+	}
+
+	// helper functions added to work with PixelFilter
 	ToArray(Input, srcx, srcy) {
 
 		var dst = new Uint32Array(srcx * srcy);
@@ -61,48 +63,56 @@ var Filter = class {
 		for (var y = 0; y < blockHeight; ++y, trgi += pitch)
 			for (var x = 0; x < blockWidth; ++x)
 				trg[trgi + x] = col;
-    }
-    
-    // from Zenju's XBRZ code
-    nearestNeighborScale(src, srcWidth, srcHeight, srcPitch, trg, trgWidth, trgHeight, trgPitch, yFirst, yLast) {
+	}
 
-        yFirst = Math.max(yFirst, 0);
-        yLast = Math.min(yLast, srcHeight);
-        
-        if (yFirst >= yLast || trgWidth <= 0 || trgHeight <= 0) return;
+	// from Zenju's XBRZ code
+	nearestNeighborScale(src, srcWidth, srcHeight, srcPitch, trg, trgWidth, trgHeight, trgPitch, yFirst, yLast) {
 
-        for (var y = yFirst; y < yLast; ++y) {
+		yFirst = Math.max(yFirst, 0);
+		yLast = Math.min(yLast, srcHeight);
 
-            //mathematically: ySrc = floor(srcHeight * yTrg / trgHeight)
-            // => search for integers in: [ySrc, ySrc + 1) * trgHeight / srcHeight
+		if (yFirst >= yLast || trgWidth <= 0 || trgHeight <= 0) return;
 
-            // keep within for loop to support MT input slices!
-            const yTrg_first = parseInt((y * trgHeight + srcHeight - 1) / srcHeight); //=ceil(y * trgHeight / srcHeight)
-            const yTrg_last = parseInt(((y + 1) * trgHeight + srcHeight - 1) / srcHeight); //=ceil(((y + 1) * trgHeight) / srcHeight)
-            const blockHeight = parseInt(yTrg_last - yTrg_first);
+		var total = yLast - yFirst;
+		var current = 0;
 
-            if (blockHeight > 0) {
+		for (var y = yFirst; y < yLast; ++y) {
 
-                var srcLine = parseInt(y * srcPitch);
-                var trgLine = parseInt(yTrg_first * trgPitch);
+			//mathematically: ySrc = floor(srcHeight * yTrg / trgHeight)
+			// => search for integers in: [ySrc, ySrc + 1) * trgHeight / srcHeight
 
-                var xTrg_first = 0;
+			// keep within for loop to support MT input slices!
+			const yTrg_first = parseInt((y * trgHeight + srcHeight - 1) / srcHeight); //=ceil(y * trgHeight / srcHeight)
+			const yTrg_last = parseInt(((y + 1) * trgHeight + srcHeight - 1) / srcHeight); //=ceil(((y + 1) * trgHeight) / srcHeight)
+			const blockHeight = parseInt(yTrg_last - yTrg_first);
 
-                for (var x = 0; x < srcWidth; ++x) {
-                    
-                    const xTrg_last = parseInt(((x + 1) * trgWidth + srcWidth - 1) / srcWidth);
-                    const blockWidth = xTrg_last - xTrg_first;
+			if (blockHeight > 0) {
 
-                    if (blockWidth > 0) {
+				var srcLine = parseInt(y * srcPitch);
+				var trgLine = parseInt(yTrg_first * trgPitch);
 
-                        xTrg_first = xTrg_last;
+				var xTrg_first = 0;
 
-                        this._FillBlock(trg, trgLine, trgPitch, src[srcLine + x], blockWidth, blockHeight);
+				for (var x = 0; x < srcWidth; ++x) {
 
-                        trgLine += blockWidth;
-                    }
-                }
+					const xTrg_last = parseInt(((x + 1) * trgWidth + srcWidth - 1) / srcWidth);
+					const blockWidth = xTrg_last - xTrg_first;
+
+					if (blockWidth > 0) {
+
+						xTrg_first = xTrg_last;
+
+						this._FillBlock(trg, trgLine, trgPitch, src[srcLine + x], blockWidth, blockHeight);
+
+						trgLine += blockWidth;
+					}
+				}
 			}
-        }
-    }
+
+			current++;
+
+			notify({ ScalingProgress: current / total * 0.8 });
+
+		}
+	}
 }
--- js/filters/resizedda.js
+++ js/filters/resizedda.js
@@ -9,12 +9,14 @@ var Filter = class {
 
         Init.Init(srcx, srcy, scale, scale, threshold);
 
-		var src = this.ToArray(Input, srcx, srcy);
+        var src = this.ToArray(Input, srcx, srcy);
         var dst = new Uint32Array(Common.SizeX * Common.SizeY);
-        
+
         this.ResizeDDA(src, dst, srcy, srcx, scale);
 
         this.ToImage(Common.ScaledImage, dst, Common.SizeX, Common.SizeY);
+
+        notify({ ScalingProgress: 1.0 });
     }
 
     getline(src, y, cols, aspline) {
@@ -35,7 +37,7 @@ var Filter = class {
 
         /* Calculate the differential amount */
         izoom = parseInt(1.0 / zoom * 1000);
-        
+
         var dstx = parseInt(zoom * cols);
 
         /* Allocate a buffer for a scan line from original image, and a
@@ -51,30 +53,30 @@ var Filter = class {
 
         /* Loop over rows in the original image */
         for (i = 0; i < rows; i++) {
-            
+
             /* Get a scan line from the original image (8-bit values expected) */
             this.getline(src, i, cols, aspline);
 
             /* Adjust the vertical accumulated differential, initialize the
             ** output X pixel and horizontal accumulated differential */
             dday -= 1000;
-            x    = 0;
+            x = 0;
             ddax = 0;
 
             /* Loop over pixels in the original image */
             for (j = 0; j < cols; j++) {
-                
+
                 /* Adjust the horizontal accumulated differential */
                 ddax -= 1000;
-                
+
                 while (ddax < 0) {
-                    
+
                     /* Store values from original image scanline into the scaled
                     ** buffer until accumulated differential crosses threshold */
                     line[x] = aspline[j];
-                    
+
                     x++;
-                    
+
                     ddax += izoom;
                 }
             }
@@ -89,57 +91,57 @@ var Filter = class {
 
                     this.SetPixel(dst, dstx, k, y, line[k]);
                 }
-                
+
                 y++;
             }
 
             current++;
 
-            notify({ScalingProgress: current / total});
+            notify({ ScalingProgress: current / total * 0.8 });
         }
     }
 
     // helper functions added to work with PixelFilter
-	ToArray(Input, srcx, srcy) {
+    ToArray(Input, srcx, srcy) {
 
-		var dst = new Uint32Array(srcx * srcy);
+        var dst = new Uint32Array(srcx * srcy);
 
-		var Channels = 4;
+        var Channels = 4;
 
-		for (var y = 0; y < srcy; y++) {
+        for (var y = 0; y < srcy; y++) {
 
-			for (var x = 0; x < srcx; x++) {
+            for (var x = 0; x < srcx; x++) {
 
-				var index = y * srcx + x;
-				var pixel = index * Channels;
+                var index = y * srcx + x;
+                var pixel = index * Channels;
 
-				var r = Input[pixel];
-				var g = Input[pixel + 1];
-				var b = Input[pixel + 2];
-				var a = Input[pixel + 3];
+                var r = Input[pixel];
+                var g = Input[pixel + 1];
+                var b = Input[pixel + 2];
+                var a = Input[pixel + 3];
 
-				dst[index] = Common.ARGBINT(a, r, g, b);
-			}
-		}
+                dst[index] = Common.ARGBINT(a, r, g, b);
+            }
+        }
 
-		return dst;
+        return dst;
     }
-    
+
     ToImage(dst, src, srcx, srcy) {
 
-		var Channels = 4;
+        var Channels = 4;
 
-		for (var y = 0; y < srcy; y++) {
-			for (var x = 0; x < srcx; x++) {
+        for (var y = 0; y < srcy; y++) {
+            for (var x = 0; x < srcx; x++) {
 
-				var index = y * srcx + x;
-				var pixel = index * Channels;
+                var index = y * srcx + x;
+                var pixel = index * Channels;
 
-				dst[pixel] = Common.Red(src[index]);
-				dst[pixel + 1] = Common.Green(src[index]);
-				dst[pixel + 2] = Common.Blue(src[index]);
-				dst[pixel + 3] = Common.Alpha(src[index]);
-			}
-		}
-	}
+                dst[pixel] = Common.Red(src[index]);
+                dst[pixel + 1] = Common.Green(src[index]);
+                dst[pixel + 2] = Common.Blue(src[index]);
+                dst[pixel + 3] = Common.Alpha(src[index]);
+            }
+        }
+    }
 }
